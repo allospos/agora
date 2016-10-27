442d7402f1e42196ded83058e7db90b062cb0bbb
Evangelos Giataganas <e.giataganas@gmail.com>
2016-10-28 00:53:20 +0100

    a user can sign in

--- spec/features/user_actions_spec.rb
+++ spec/features/user_actions_spec.rb
@@ -1,33 +1,51 @@
 require 'rails_helper'
 
-RSpec.feature "Sign up" do
-  before do
-    visit root_path
-    click_on "Sign up"
-  end
-
-  it "a user cannot sign up until they have all mandatory fields filled" do
-    fill_in 'Email', with: 'user@example.com'
-    fill_in 'Password', with: 'password'
-    click_button 'Sign up'
-
-    within(".user_username") do
-      expect(page).to have_content "can't be blank"
+RSpec.feature 'User Actions' do
+  feature 'a user click Sign up' do
+    before do
+      visit root_path
+      click_on 'Sign up'
     end
 
-    within(".user_full_name") do
-      expect(page).to have_content "can't be blank"
+    scenario 'and cannot sign up until they have all mandatory fields filled' do
+      fill_in 'Email', with: 'user@example.com'
+      fill_in 'Password', with: 'password'
+      click_button 'Sign up'
+
+      within('.user_username') do
+        expect(page).to have_content "can't be blank"
+      end
+
+      within('.user_full_name') do
+        expect(page).to have_content "can't be blank"
+      end
+    end
+
+    scenario 'and can sign up succesfuly when they complete all required fields' do
+      fill_in 'Full name', with: 'Jane Doe'
+      fill_in 'Username', with: 'janed'
+      fill_in 'Email', with: 'user@example.com'
+      fill_in 'Password', with: 'password'
+      click_button 'Sign up'
+
+      expect(page).to have_content('Signed in as: user@example.com')
     end
   end
 
-  it "a user can sign up succesfuly when they complete all required fields" do
-    fill_in 'Full name', with: 'Jane Doe'
-    fill_in 'Username', with: 'janed'
-    fill_in 'Email', with: 'janed@example.com'
-    fill_in 'Password', with: 'password'
-    click_button 'Sign up'
+  feature 'a user click Sign in' do
+    let!(:user) { User.create(full_name: 'Jane Doe', username: 'janed', email: 'janed@example.com', password: 'password') }
 
-    expect(page).to have_content("Signed in as: janed@example.com")
+    before do
+      visit root_path
+      click_on 'Sign in'
+    end
+
+    scenario 'and can sign up succesfuly when they complete all required fields' do
+      fill_in 'Email', with: 'janed@example.com'
+      fill_in 'Password', with: 'password'
+      click_button 'Sign in'
+
+      expect(page).to have_content('Signed in as: janed@example.com')
+    end
   end
 end
-
