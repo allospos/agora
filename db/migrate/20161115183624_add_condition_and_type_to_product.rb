de632fc5c139e30f4ffa789cc4034828b2d927c4
Despo Pentara <despo@despo.me>
2016-11-15 19:28:46 +0000

    add condition and product_type to product

---
+++ db/migrate/20161115183624_add_condition_and_type_to_product.rb
@@ -0,0 +1,6 @@
+class AddConditionAndTypeToProduct < ActiveRecord::Migration[5.0]
+  def change
+    add_column :products, :condition, :string
+    add_column :products, :product_type, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028000938) do
+ActiveRecord::Schema.define(version: 20161115183624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,10 @@ ActiveRecord::Schema.define(version: 20161028000938) do
     t.text     "description"
     t.integer  "user_id"
     t.string   "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "condition"
+    t.string   "product_type"
     t.index ["user_id"], name: "index_products_on_user_id", using: :btree
   end
 
