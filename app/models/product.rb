d2cf3fb1087d5bb747d8aa83c6cd4065f20175b6
Despo Pentara <despo@despo.me>
2016-11-17 01:17:27 +0000

    add ‘small’ version of image for landing page ensure title,price and description are present

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,18 +4,17 @@ class Product < ApplicationRecord
   has_many :product_delivery_methods
   has_many :delivery_methods, through: :product_delivery_methods, source: :delivery_method
 
-  validates :user, presence:  true
-  validates :condition, presence:  true
+  validates :condition, :title, :description, :price, :user, presence:  true
   validate :delivery_method_count_validation
 
   mount_uploaders :images, ImageUploader
-
 
   def shortened_title
     title.length > 60 ? "#{title[0...54]}..." : title
   end
 
   private
+
   def delivery_method_count_validation
     if delivery_methods.length < 1
       errors.add(:delivery_methods, "You have to select at least one delivery method")
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,13 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   context "#validations" do
-    let(:product) { Fabricate.build(:product, delivery_methods: []) }
+
+    it "must have a title set" do
+      product = Fabricate.build(:product, title: nil)
+      product.save
+
+      expect(product.errors.messages).to include(:title)
+    end
+
+    it "must have a description set" do
+      product = Fabricate.build(:product, description: nil)
+      product.save
+
+      expect(product.errors.messages).to include(:description)
+    end
+
+    it "must have a price set" do
+      product = Fabricate.build(:product, price: nil)
+      product.save
+
+      expect(product.errors.messages).to include(:price)
+    end
 
     it "must have at least one delivery method set" do
       product = Fabricate.build(:product, delivery_methods: [])
       product.save
 
-      expect(product).to_not be_valid
       expect(product.errors.messages).to include(:delivery_methods)
     end
 
@@ -16,7 +35,6 @@ RSpec.describe Product, type: :model do
       product = Fabricate.build(:product, condition: nil)
       product.save
 
-      expect(product).to_not be_valid
       expect(product.errors.messages).to include(:condition)
     end
 
