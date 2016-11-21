35a6b175461aad635ec88e7e3cfc7fde5f322978
Despo Pentara <despo@despo.me>
2016-11-21 14:36:38 +0000

    show user page - link from product

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'dashboard#index'
 
   post "/users", to: "users#create"
+  resources :users, only: [:show]
 
   resources :products, only: [:index, :create, :new, :show] do
     resources :favourites, only: [:create]
